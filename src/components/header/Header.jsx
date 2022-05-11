02c121255a2bce5a8987f9471d06c954e706a7c0
Amen Kemal <57805638+mike964@users.noreply.github.com>
2022-05-12 00:48:55 +0300

    header navbar background-color changed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -15,7 +15,11 @@ const Header = () => {
 
 	return (
 		<header>
-			<Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+			<Navbar
+				// bg='dark'
+				variant='dark'
+				expand='lg'
+				collapseOnSelect>
 				<Container>
 					<LinkContainer to='/'>
 						<Navbar.Brand className='text-light'>
